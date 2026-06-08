ca5938a3784c575d6841e1df0f5fe2a6ed99d80f
Ismael <id386827@gmail.com>
2026-06-09 00:51:27 +0200

    fix: resolver hidratacion de fechas y modulo crypto en edge manteniendo diseño estable

--- components/DateDisplay.tsx
+++ components/DateDisplay.tsx
@@ -1,5 +1,5 @@
 'use client'
 
 export default function DateDisplay({ date }: { date: string | Date }) {
-  return <span suppressHydrationWarning>{new Date(date).toLocaleDateString()}</span>
+  return <span suppressHydrationWarning>{new Date(date).toLocaleDateString('es-ES')}</span>
 }
